edc385a26c2a3be59deecb59832874ce2872eb79
crystalize-dev <igandreev@ptsecurity.com>
2024-07-03 13:52:11 +0300

    Init refactor

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,33 +1,43 @@
-import React from 'react';
-import cl from "./Header.module.css"
+import React from "react";
+import cl from "./Header.module.css";
 import Link from "../link/Link";
-import classNames from 'classnames';
+import classNames from "classnames";
 import Icon from "../icon/icon";
 import ThemeSwitcher from "../themeSwitcher/themeSwitcher";
 import { useNavigate } from "react-router-dom";
 
-
-const Header = ({header, setHeader}) => {
-
-    const navigate = useNavigate()
+const Header = ({ header, setHeader }) => {
+    const navigate = useNavigate();
 
     return (
-        <header className={header ? classNames(cl.header, cl.show) : classNames(cl.header, cl.hide)}
-                onClick={(e) => e.stopPropagation()}>
+        <header
+            className={
+                header
+                    ? classNames(cl.header, cl.show)
+                    : classNames(cl.header, cl.hide)
+            }
+            onClick={(e) => e.stopPropagation()}
+        >
             <div className={cl.container}>
-                <Link to="/" >Description</Link>
+                <Link to="/">Description</Link>
                 <Link to="Characters">Characters</Link>
 
-                <h1 onClick={() => navigate('/')}>DEMON SLAYER</h1>
-                <div className={header ? classNames(cl.expander) : classNames(cl.expander, cl.rotate)}
-                     onClick={() => setHeader(!header)}>
+                <h1 onClick={() => navigate("/")}>DEMON SLAYER</h1>
+                <div
+                    className={
+                        header
+                            ? classNames(cl.expander)
+                            : classNames(cl.expander, cl.rotate)
+                    }
+                    onClick={() => setHeader(!header)}
+                >
                     <Icon>expand_less</Icon>
                 </div>
 
                 <Link to="Reviews">Reviews</Link>
-                <Link to="About" >About me</Link>
+                <Link to="About">About me</Link>
 
-                <ThemeSwitcher/>
+                <ThemeSwitcher />
             </div>
         </header>
     );
--- src/pages/Description/Description.jsx
+++ src/pages/Description/Description.jsx
@@ -1,47 +1,60 @@
-import React, {useContext, useState} from 'react';
-import cl from "./Description.module.css"
+import React, { useState } from "react";
+import cl from "./Description.module.css";
 import Icon from "../../components/icon/icon";
 import classNames from "classnames";
-import {ThemeContext} from "../../context/themeContext";
-import logo from "../../img/logo.png"
+import logo from "../../img/logo.png";
 
 const Description = () => {
-    const [fullscreen, setFullScreen] = useState(false)
-    const {theme, switchTheme} = useContext(ThemeContext)
-
-    const goFullScreen = () => {
-        if (theme !== "dark") switchTheme();
-
-        setFullScreen(!fullscreen);
-    }
+    const [fullscreen, setFullScreen] = useState(false);
 
     return (
         <div className={cl.wrapper}>
-            <div className={fullscreen ? classNames(cl.videoWrapper, cl.fullScreen) : cl.videoWrapper}>
-                <iframe src="https://www.youtube.com/embed/VQGCKyvzIM4?start=6" allowFullScreen className={cl.video} title={"video"}/>
+            <div
+                className={
+                    fullscreen
+                        ? classNames(cl.videoWrapper, cl.fullScreen)
+                        : cl.videoWrapper
+                }
+            >
+                <iframe
+                    src="https://www.youtube.com/embed/VQGCKyvzIM4?start=6"
+                    allowFullScreen
+                    className={cl.video}
+                    title={"video"}
+                />
 
                 <div className={cl.fullScreenButton}>
-                    <Icon onClick={() => goFullScreen()}>{fullscreen ? "fullscreen_exit" : "fullscreen"}</Icon>
+                    <Icon onClick={() => setFullScreen(!fullscreen)}>
+                        {fullscreen ? "fullscreen_exit" : "fullscreen"}
+                    </Icon>
                 </div>
             </div>
 
             <div className={cl.descriptionWrapper}>
-                <img src={logo} alt={"logo"}/>
-                <hr/>
+                <img src={logo} alt={"logo"} />
+                <hr />
                 <p>
-                    The story takes place in Taishō era Japan, where a secret society, known as the Demon Slayer Corps,
-                    has been waging a secret war against demons for centuries. The demons are former humans who were
-                    turned into demons by Muzan Kibutsuji injecting them with his own blood, and they feed on humans and
-                    possess supernatural abilities such as super strength, powers that demons can obtain called "Blood
-                    Demon Art", and regeneration. Demons can only be killed if they are decapitated with weapons crafted
-                    from an alloy known as Nichirin, injected with poison extracted from wisteria flowers, or exposed to
-                    sunlight. The Demon Slayers, on the other hand, are entirely human; however, they employ special
-                    elemental breathing techniques, known as "Breathing Styles", which grant them superhuman strength
-                    and increased resistance, helping them fight against demons. The strongest Demon Slayers are known
-                    as the Hashira, and gain this rank through either killing a member of the Twelve Kizuki (the twelve
-                    strongest demons under Muzan) or killing fifty demons after ranking up multiple times.
+                    The story takes place in Taishō era Japan, where a secret
+                    society, known as the Demon Slayer Corps, has been waging a
+                    secret war against demons for centuries. The demons are
+                    former humans who were turned into demons by Muzan Kibutsuji
+                    injecting them with his own blood, and they feed on humans
+                    and possess supernatural abilities such as super strength,
+                    powers that demons can obtain called "Blood Demon Art", and
+                    regeneration. Demons can only be killed if they are
+                    decapitated with weapons crafted from an alloy known as
+                    Nichirin, injected with poison extracted from wisteria
+                    flowers, or exposed to sunlight. The Demon Slayers, on the
+                    other hand, are entirely human; however, they employ special
+                    elemental breathing techniques, known as "Breathing Styles",
+                    which grant them superhuman strength and increased
+                    resistance, helping them fight against demons. The strongest
+                    Demon Slayers are known as the Hashira, and gain this rank
+                    through either killing a member of the Twelve Kizuki (the
+                    twelve strongest demons under Muzan) or killing fifty demons
+                    after ranking up multiple times.
                 </p>
-                <hr/>
+                <hr />
             </div>
         </div>
     );
